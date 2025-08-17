39fcb92f28da5602ffa18efa8de366e658a3b876
Dishant <shoonyam.108@gmail.com>
2025-08-17 19:15:58 +0530

    bit progress on the profile page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
+      <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
+        <form className='flex flex-col gap-5 p-10 flex-1'>
+          <h3 className='text-lg'>Profile details</h3>
+          <label htmlFor="avatar"></label>
+        </form>
+        <img src="" alt="" />
+      </div>
+    </div>
   )
 }
 
