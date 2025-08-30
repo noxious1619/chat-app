2a7a407e9bf325dd824a3abe3e9c3a3d74be791c
Dishant <shoonyam.108@gmail.com>
2025-08-30 18:10:23 +0530

    done with most of the backend

--- server/server.js
+++ server/server.js
@@ -4,10 +4,38 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { Server } from "socket.io";
+
 
 //create server
 const app = express();
 const server = http.createServer(app);
+
+//Initialize socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+})
+
+//store online user
+export const userSocketMap = {}; //{userId: socketId }
+
+// socket.io connection Handler
+io.on("connection", (socket)=> { 
+    const userId socket.handshake.query.userId;
+    console.log("user connected");
+
+    if (userId) = userSocketMap[userId] = socket.id;
+
+    //Emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", ()=>{
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+    })
+})
 
 //Middleware setup
 app.use(express.json({ limit: "4mb" }));
